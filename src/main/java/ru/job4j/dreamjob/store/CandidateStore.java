9295098e08c0d348f43fb9c7878ca8f9de335b15
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-07-01 17:18:44 +0400

    [#504842]: - implemented logic for list of candidates in table;

---
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -0,0 +1,27 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Candidate;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+    private static final CandidateStore STORE = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    public CandidateStore() {
+        candidates.put(1, new Candidate(1, "JAVA Software Developer", "Scream fiery like a black tobacco."));
+        candidates.put(2, new Candidate(2, "Jr. JAVA v.11 Developer Associate", "The sun acquires."));
+        candidates.put(3, new Candidate(3, "JAVA Developer", "The transformator is more pathway now than species. calm and unearthly harmless."));
+    }
+
+    public static CandidateStore instOf() {
+        return STORE;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
